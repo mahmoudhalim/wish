4aac14620de4f53f45bffe35f779fb141d8b27b0
mahmoud isa <mahmoudahalim0@gmail.com>
2024-01-30 21:57:28 +0200

    Add support for batch mode

--- wish.c
+++ wish.c
@@ -5,8 +5,7 @@
 #include <string.h>
 size_t BUFFERSIZE = 64;
 
-char *PATH[128] = {
-    "/bin/"};
+char *PATH[128] = {};
 
 int parse_input(char *myargv[], char *buffer, size_t n)
 {
@@ -14,9 +13,9 @@ int parse_input(char *myargv[], char *buffer, size_t n)
   int myargc = 0;
   char *s = (char *)malloc(sizeof(strlen(buffer)));
 
-  // remove trailling newline character
-  if(buffer[n - 1] == '\n')
-  { 
+  // remove trailing newline character
+  if (buffer[n - 1] == '\n')
+  {
     buffer[n - 1] = '\0';
     n--;
   }
@@ -29,7 +28,7 @@ int parse_input(char *myargv[], char *buffer, size_t n)
       myargc++;
     }
   }
-   myargv[i] = NULL;
+  myargv[i] = NULL;
   // s = strdup(myargv[0]);
   // myargv[0] = strdup(PATH[0]);
   // myargv[0] = strcat(myargv[0], s);
@@ -37,35 +36,68 @@ int parse_input(char *myargv[], char *buffer, size_t n)
   return myargc;
 }
 
-
-int get_path(char* myargv[]){
+int get_path(char *myargv[])
+{
   char temp_path[64];
   int i = 0;
   while (1)
   {
     if (PATH[i] == NULL)
       break;
-    else {
+    else
+    {
       strcpy(temp_path, PATH[i]);
       strcat(temp_path, myargv[0]);
       int acc_rc = access(temp_path, X_OK);
-      if (acc_rc == 0)  
+      if (acc_rc == 0)
       {
         myargv[0] = strdup(temp_path);
         return 0;
       }
-        }
-        i++;
+    }
+    i++;
   }
+  strcpy(temp_path, "/bin/");
+  strcat(temp_path, myargv[0]);
+  if (access(temp_path, X_OK) == 0)
+  {
+    return 0;
+  }
+
   return -1;
 }
 
-int main(int argc, char *argv[]){
-  while (1){
-    printf("wish> ");
+int main(int argc, char *argv[])
+{
+  if (argc > 3)
+  {
+    fprintf(stderr, "An error has occurred\n");
+    return 1;
+  }
+
+  if (argc == 2)
+  {
+    FILE *f = fopen(argv[1], "r");
+    dup2(fileno(f), STDIN_FILENO);
+  }
+
+  while (1)
+  {
+    if (argc == 1)
+    {
+      printf("wish> ");
+    }
+
     char *buffer = (char *)malloc(sizeof(BUFFERSIZE));
     size_t n = getline(&buffer, &BUFFERSIZE, stdin);
     char *myargv[128];
+
+    // empty input
+    if (n == 1)
+    {
+      continue;
+    }
+
     int myargc = parse_input(myargv, buffer, n);
     // handle exit
     if (strcmp(myargv[0], "exit") == 0)
@@ -102,24 +134,26 @@ int main(int argc, char *argv[]){
       if (ret == -1)
       {
         fprintf(stderr, "An error has occurred\n");
-        continue;
       }
-                    }
+      continue;
+    }
 
-    if (get_path(myargv) == -1){
+    if (get_path(myargv) == -1)
+    {
       fprintf(stderr, "An error has occurred\n");
       continue;
     }
 
-      int rc = fork();
-    if (rc == 0) { // child process
-      rc = execv(myargv[0], myargv);
-      printf("%s ",myargv[0]);
+    int rc = fork();
+    if (rc == 0)
+    { // child process
+      rc = execvp(myargv[0], myargv);
       fprintf(stderr, "An error has occurred\n");
     }
-  else{
-    // parent 
-    rc = wait(NULL);
-  }
+    else
+    {
+      // parent
+      wait(NULL);
+    }
   }
 }
